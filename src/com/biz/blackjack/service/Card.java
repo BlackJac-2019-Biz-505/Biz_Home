0a4c9227c211fb73bc6a01c78179eda9b4fe8e59
doolybom1 <elqkqkwkd@gmail.com>
2019-09-25 02:03:09 +0900

    shuffle

--- src/com/biz/blackjack/service/Card.java
+++ src/com/biz/blackjack/service/Card.java
@@ -1,5 +1,9 @@
 package com.biz.blackjack.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Card {
@@ -7,23 +11,30 @@ public class Card {
 	Random rnd;
 	String[] card = new String[] { "A", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "K",
 			"Q", "J" };
+	List<String> dupliDelete;
 	String[] figure = new String[] { "diamond", "heart", "Spade", "clover" };
 
 	public Card() {
 		rnd = new Random();
+		dupliDelete = new ArrayList<String>();
 	}
 
 	public void shuffle() {
 		int cardShuffle = 0;
 		int figureShuffle = 0;
-
-		for (int i = 0; i < figure.length; i++) {
-			figureShuffle = rnd.nextInt(3);
-			for (int j = 0; j < card.length; j++) {
-				cardShuffle = rnd.nextInt(13);				
-				System.out.println("["+figure[figureShuffle]+"]" + card[cardShuffle]);
-			}
+		int rndValue = 13;
+		Collections.addAll(dupliDelete, card);
+		System.out.println(dupliDelete);
+		int nSize = dupliDelete.size();
+		
+		for (int i = 0; i < nSize  ; i++) {
+			cardShuffle = rnd.nextInt(rndValue);
+			System.out.println(cardShuffle);
+			dupliDelete.remove(cardShuffle);
+			System.out.println(dupliDelete);
+			nSize--;
+			rndValue--;
+			i = 0;
 		}
 	}
-
 }
